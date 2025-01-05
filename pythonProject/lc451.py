5ea43257b4ea57957e663a3336918638d69a91ea
LogicalGuy77 <harshitacademia@gmail.com>
2025-01-06 00:16:36 +0530

    lc451

--- pythonProject/lc451.py
+++ pythonProject/lc451.py
@@ -1,2 +1,16 @@
+from collections import Counter
+from collections import defaultdict
 s = "tree"
 
+count = Counter(s)
+buckets = defaultdict(list)
+
+for char, cnt in count.items():
+    buckets[cnt].append(char)
+
+res = ""
+
+for i in range(len(s), 0, -1):
+    for letter in buckets[i]:
+        res += letter * i
+print(res)
